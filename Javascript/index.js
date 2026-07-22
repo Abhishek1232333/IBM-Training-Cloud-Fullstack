de7633d441c72449002df018042474fd942893f3
Abhishek1232333 <goudabhishekgoud@gmail.com>
2026-07-22 05:44:20 +0000

    javascript codes

---
+++ Javascript/index.js
@@ -0,0 +1,107 @@
+//console.log("Hello Abhishek");
+
+let a=5;
+let b=11.2;
+
+// console.log(a+b);
+//console.log(typeof(a));
+
+let arr=[1,2.3,55,"IBM"]
+// console.log(typeof(arr));
+// console.log(arr);
+
+// for(let i of arr){
+//     console.log(i);
+    
+// }
+
+// for(let i in arr){
+//     console.log(arr[i]);
+// }
+
+//function
+// function sum(a,b){
+//     console.log(a+b);
+    
+// }
+// sum(a,b);
+
+
+// let x=5;
+// var y=10;
+
+// {
+//     let x=20;
+//     var y=22;
+//     console.log("Inside block:",x);
+//     console.log("Inside block:",y);
+    
+// }
+//  console.log("oustside block:",x);
+//  console.log("oustside block:",y);
+
+
+//String functions
+
+let str="Bangalore"
+
+// console.log(str.length);
+// console.log(str.split());
+// console.log(`I live in ${str}`);
+
+// console.log(arr.indexOf("IBM"));
+// console.log(arr+arr);
+
+
+// arr.forEach(e=>console.log(e)
+// );
+
+// s=(a,b)=>{
+//     return a-b;
+// }
+
+// console.log(s(a,b));
+
+//object creation::
+// let obj={
+//     "name":"Abhi",
+//     "Role":"ASE",
+
+// }
+
+// console.log(obj);
+
+// let cal={
+//     "sum":(a,b)=>a+b,
+//     "sub":(a,b)=>a-b,
+//     "mul":(a,b)=>a*b,
+//     "div":(a,b)=>a/b,
+// }
+
+// console.log(cal.sum(a,b));
+
+
+//Filter method
+
+// arr.filter((e)=>e%2!=0).forEach((e)=>console.log(e)
+// )
+
+// arr.map((e)=>e*2).forEach((e)=>console.log(e)
+// )
+
+// arr.map((e)=>e*2).forEach((e)=>console.log(e)
+// )
+
+// arr.filter((e)=>typeof(e)=="number").map((e)=>e*10).forEach((e)=>console.log(e)
+// )
+
+
+
+
+
+
+
+
+
+
+
