1b0bbc9f42452502277109a1018ca6cf2ffa774a
Abhishek1232333 <goudabhishekgoud@gmail.com>
2026-07-24 03:45:59 +0000

    mongo-setup

---
+++ nodemongo/db.js
@@ -0,0 +1,16 @@
+const mongo=require("mongoose")
+require('dotenv').config();
+
+const connectDB=async ()=>{
+    try {
+        const conn = await mongo.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+};
+
+
+module.exports = connectDB;
